b9e60b7d8ac39a9f0e932e857db67e0ac5f10bd1
xhetry-ashish <ashishxhetry2017@gmail.com>
2022-04-16 21:42:14 +0545

    route component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import Header from './components/Header/header'; 
-import Projects from './components/ProjectCard/projects';
-import Contact from './pages/Contact/contact';
-import Resume from './pages/Resume/resume';
-import Testimonial from './pages/Testimonial/testimonial'
-import{BrowserRouter,Routes,Route} from 'react-router-dom';
-import Profile from './components/Profile/profile';
-import {profileData} from './utils/Constants/profile'
-import {headerData} from './utils/Constants/header'
+import RoutePath from './components/Route/route'
 
 function App() {
   return (
-      <BrowserRouter>
-      <Header data={headerData['profile-1']}/>
-      <Routes>
-        <Route path="/" exact element={<Profile data={profileData['profile-1']}/>}/>
-        <Route path="/home" element={<Profile data={profileData['profile-1']} />}/>
-        <Route path="/project" element={<Projects/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/testimonial" element={<Testimonial/>}/>
-        <Route path="/resume" element={<Resume/>}/>
-      </Routes>
-      </BrowserRouter>
+     <RoutePath ></RoutePath>
   );
 }
 
