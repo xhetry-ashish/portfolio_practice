5ea7ed215883bfcde57f6c2cecbba3cdf369762c
xhetry-ashish <ashishxhetry2017@gmail.com>
2022-04-22 08:46:10 +0545

    theme color changed

--- src/components/Icon/icon.jsx
+++ src/components/Icon/icon.jsx
@@ -3,16 +3,14 @@ import styled from "styled-components";
 const Icon = styled.i`
   
   background-color: transparent;
-  border: 1px dashed black;
-  border-radius: 20%;
+  //border: 1px dashed black;
+  //border-radius: 20%;
   margin: 1rem;
-  color: yellow;
+  color: #0e6d67;
   padding: 1rem;
-  font-size: 1rem;
+  font-size: 2rem;
   cursor: pointer;
   &:hover {
-    //background-color: $text-color;
-    //color: #140f0f;
     transform: scale(1.05);
   }
 `;
