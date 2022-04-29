87fc8f44659d2d207243449ebe3ec90fd7cc2bd6
xhetry-ashish <ashishxhetry2017@gmail.com>
2022-04-29 22:22:13 +0545

    header component updated

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import "./header.scss";
-//import toggleImage from "../../assets/image/sun.jpg";
-//import logoImage from "../../assets/image/logo1.png";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header(props) {
   const [isMobile, setMobile] = useState(false);
@@ -23,13 +21,13 @@ export default function Header(props) {
           >
             <ul>
               <li>
-                <Link to={props.data.link1Path}>{props.data.link1}</Link>
+                <NavLink  to={props.data.link1Path}>{props.data.link1}</NavLink>
               </li>
               <li>
-                <Link to={props.data.link2Path}>{props.data.link2}</Link>
+                <NavLink to={props.data.link2Path}>{props.data.link2}</NavLink>
               </li>
               <li>
-                <Link to={props.data.link3Path}>{props.data.link3}</Link>
+                <NavLink to={props.data.link3Path}>{props.data.link3}</NavLink>
               </li>
             </ul>
           </div>
