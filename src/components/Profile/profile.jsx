17e74061978870953fb4188b34a0f3fc519edd6a
xhetry-ashish <ashishxhetry2017@gmail.com>
2022-04-29 22:35:20 +0545

    profile section and icon updated

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,58 +1,70 @@
 import React from "react";
-import emoji from "react-easy-emoji";
 import Button from "../common/Button/button";
 import "./profile.scss";
 import Icon from "../common/Icon/icon";
 import { Link } from "react-router-dom";
 
-
-
 function Profile(props) {
- 
   return (
     <div className="profile-container">
       <div className="profile-parent">
         <div className="profile-details">
-          <div className="colz">
-            <div className="colz-icon">
-              <a href={props.data.facebookLink} target="_blank" rel="noreferrer">
-                <Icon className="fa fa-facebook"></Icon>
-              </a>
-              <a href={props.data.linkedinLink} target="_blank" rel="noreferrer">
-              <Icon className="fa fa-linkedin"></Icon>
-              </a>
-              <a href={props.data.githubLink} target="_blank" rel="noreferrer">
-              <Icon className="fa fa-github"></Icon>
-              </a>
-              <a href={props.data.instagramLink} target="_blank" rel="noreferrer">
-              <Icon className="fa fa-instagram"></Icon>
-              </a>
-            </div>
-          </div>
-          
           <div className="profile-details-name">
             <span className="primary-text">
-              Hello, It's me <span className="highlighted-text">{props.data.name}</span>
+              Hello, It's me 
+              <span className="highlighted-text">  {props.data.name}</span>
             </span>
           </div>
-          <span className="wave-emoji">{emoji("👋")}</span>
           <div className="profile-details-role">
             <span className="primary-text">
-              <h1>{props.data.profileRole}
-              </h1>
+              <h1>{props.data.profileRole}</h1>
               <span className="profile-role-tagline">
                 {props.data.profileTagline}
               </span>
             </span>
           </div>
           <div className="profile-options">
-           <Link to='/resume'> <Button>Know Me</Button></Link>
-           <Link to='/contact'> <Button>Hire Me</Button></Link>
+            <Link to="/resume">
+              {" "}
+              <Button>Know Me</Button>
+            </Link>
+            <Link to="/contact">
+              {" "}
+              <Button>Hire Me</Button>
+            </Link>
+          </div>
+          <div className="colz">
+            <div className="colz-icon">
+              <a
+                href={props.data.facebookLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icon className="fa fa-facebook"></Icon>
+              </a>
+              <a
+                href={props.data.linkedinLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icon className="fa fa-linkedin"></Icon>
+              </a>
+              <a href={props.data.githubLink} target="_blank" rel="noreferrer">
+                <Icon className="fa fa-github"></Icon>
+              </a>
+              <a
+                href={props.data.instagramLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icon className="fa fa-instagram"></Icon>
+              </a>
+            </div>
           </div>
         </div>
         <div className="profile-picture">
           <div className="profile-picture-background">
-            <img src= {props.data.Image} alt="no loading"/>
+            <img src={props.data.Image} alt="no loading" />
           </div>
         </div>
       </div>
--- src/components/common/Icon/icon.jsx
+++ src/components/common/Icon/icon.jsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 const Icon = styled.i`
   
   background-color: transparent;
-  //border: 1px dashed black;
-  //border-radius: 20%;
-  margin: 1rem;
+  border: 1px solid black; 
+  border-radius: 20%;
+  margin: 1.5rem;
   color: #7B3E16;
-  padding: 1rem;
-  font-size: 2rem;
+  padding: 1.2rem;
+  font-size: 1.5rem;
   cursor: pointer;
   &:hover {
     transform: scale(1.05);
